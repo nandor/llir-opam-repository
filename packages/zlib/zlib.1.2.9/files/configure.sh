5aa5d2c690cc9892ab3f777d91faea29abe5fd0e
Nandor Licker <n@ndor.email>
2020-11-07 23:44:57 +0000

    [llir-opam-repository] Fixed arm64 build

--- packages/zlib/zlib.1.2.9/files/configure.sh
+++ packages/zlib/zlib.1.2.9/files/configure.sh
@@ -1,29 +1,5 @@
 #!/bin/sh
 
+host=`cat $prefix/lib/ocaml/Makefile.config | grep '^ARCH' | sed -e s/ARCH=//g`
 
-
-prefix=/usr
-host=none
-
-while : ; do
-  case "$1" in
-    "") break;;
-    --prefix)
-      prefix=$2; shift;;
-    --host)
-      host=$2; shift;;
-    *)
-      >&2 echo "Unknown option \"$1\".";;
-  esac
-  shift
-done
-
-llir=`cat $prefix/lib/ocaml/Makefile.config | grep LLIR | sed -e s/LLIR=//g`
-
-if test x"$llir" = xtrue; then
-  host="llir_$host"
-else
-  host=""
-fi
-
-CHOST=$host ./configure --prefix $prefix
+CHOST=$host-pc-linux-musl ./configure $@
